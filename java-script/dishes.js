0c6b332f06985ba83f31829aa3cf690fb7f7bac7
voan24ge <voan24ge@student.ju.se>
2025-04-20 22:37:45 +0200

    main dishes html page, css styling and js to display the elements from json. new images size

--- java-script/dishes.js
+++ java-script/dishes.js
@@ -12,14 +12,19 @@ function displayRecipes(recipes) {
   const recipeListContainer = document.getElementById("recipe-list");
 
   recipes.forEach((recipe) => {
-    const recipeInfo = document.createElement("div");
-    recipeInfo.classList.add("recipe-info");
+    // Recipe card container
+    const recipeCard = document.createElement("div");
+    recipeCard.classList.add("recipe-card");
 
     // Image
-    const imageContainer = document.createElement("img");
-    imageContainer.classList.add("recipe-image");
-    imageContainer.src = recipe.image;
-    recipeInfo.appendChild(imageContainer);
+    const image = document.createElement("img");
+    image.src = recipe.image;
+    image.alt = recipe.name;
+    image.classList.add("recipe-image");
+
+    // Recipe info container (for name + prep time)
+    const recipeInfo = document.createElement("div");
+    recipeInfo.classList.add("recipe-info");
 
     // Name
     const nameContainer = document.createElement("h2");
@@ -31,8 +36,21 @@ function displayRecipes(recipes) {
     prepContainer.textContent = recipe.time;
     recipeInfo.appendChild(prepContainer);
 
-    // Add the recipe information to the list
-    recipeListContainer.appendChild(recipeInfo);
+    // Save button
+    const saveIcon = document.createElement("i");
+    saveIcon.classList.add("fa-regular", "fa-bookmark", "save-icon");
+    saveIcon.addEventListener("click", () => {
+      saveIcon.classList.toggle("fa-regular");
+      saveIcon.classList.toggle("fa-solid");
+    });
+
+    // Append info and icon to card
+    recipeCard.appendChild(image);
+    recipeCard.appendChild(recipeInfo);
+    recipeCard.appendChild(saveIcon);
+
+    // Add the full recipe card to the list
+    recipeListContainer.appendChild(recipeCard);
   });
 }
 
